94c93a915ef1d346ec8d64e48a40fb6156352547
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2022-10-21 17:02:04 +0700

    complete page product, list product, login
    21/10 complete pages Product, ProductList, Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
       </div>
     </Router>
